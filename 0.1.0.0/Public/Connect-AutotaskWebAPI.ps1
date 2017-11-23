9da0d8330650995e4d4acbc781df782659ceaae0
Hugo Klemmestad <hugo@office-center.no>
2017-11-23 10:40:58 +0100

    Updated a bit..

--- 0.1.0.0/Public/Connect-AutotaskWebAPI.ps1
+++ 0.1.0.0/Public/Connect-AutotaskWebAPI.ps1
@@ -1,10 +1,43 @@
-﻿Function Connect-AutotaskWebAPI
+﻿<#
+
+    .COPYRIGHT
+    Copyright (c) Office Center Hønefoss AS. All rights reserved. Licensed under the MIT license.
+    See https://github.com/officecenter/OCH-Public/blob/master/LICENSE for license information.
+
+#>
+
+Function Connect-AutotaskWebAPI
 {
+   <#
+      .SYNOPSIS
+      This function connects to the Autotask Web Services API.
+      .DESCRIPTION
+      The function takes a credential object and uses it to authenticate and connect to the Autotask
+      Web Services API
+      .INPUTS
+      A PSCredential object. Required. It will prompt for credentials if the object is not provided.
+      .OUTPUTS
+      A webserviceproxy object is created.
+      .EXAMPLE
+      Connect-AutotaskWebAPI
+      Prompts for a username and password and authenticates to Autotask
+      .EXAMPLE
+      Connect-AutotaskWebAPI
+      .NOTES
+      NAME: Connect-AutotaskWebAPI
+      .LINK
+      Get-AtwsData
+      New-AtwsQuery
+  #>
+	
     [cmdletbinding()]
     Param
     (
         [pscredential]
-        $Credential = $(Get-Credential -Message 'Autotask Web Services API login')
+        $Credential = $(Get-Credential -Message 'Autotask Web Services API login'),
+        
+        [Switch]
+        $Silent = $false
     )
     
     Do
@@ -21,20 +54,16 @@
         $ZoneInfo = $RootService.getZoneInfo($Credential.UserName)
         If ($ZoneInfo.ErrorCode -ne 0)
         {
-            Write-Error 'Invalid username "{0}". Try again.' -f $User
+            Write-Error ('Invalid username "{0}". Try again.' -f $User)
             $Credential = $(Get-Credential -Message 'Autotask Web Services API login')
         }
     }
-    Until ($ZoneInfo.ErrorCode -eq 0)
+    Until ($ZoneInfo.ErrorCode -eq 0 -or $Silent)
     
     $Uri = $ZoneInfo.URL -replace 'atws.asmx','atws.wsdl'
     
     # Make sure a failure to create this object truly fails the script
-    #$PreviousPreference = $ErrorActionPreference
-    #$ErrorActionPreference = 'STOP'
     $global:atws = New-WebServiceProxy -URI $Uri  -Credential $credential -Namespace 'Autotask' -Class 'AutotaskAPI' -ErrorAction Stop
     
-    # Return the user to his preference
-    #$ErrorActionPreference = $PreviousPreference
-    
+    Return $global:atws
 }
