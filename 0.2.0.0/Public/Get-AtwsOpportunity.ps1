352d08fbcc4558a1e7113625f804aca942366280
Hugo Klemmestad <hugo@office-center.no>
2017-12-06 10:30:58 +0100

    Fixed validateset with special character in label

---
+++ 0.2.0.0/Public/Get-AtwsOpportunity.ps1
@@ -0,0 +1,528 @@
+﻿<#
+
+.COPYRIGHT
+Copyright (c) Office Center Hønefoss AS. All rights reserved. Based on code from Jan Egil Ring (Crayon). Licensed under the MIT license.
+See https://github.com/officecenter/OCH-Public/blob/master/LICENSE for license information.
+
+#>
+
+Function Get-AtwsOpportunity
+{
+  <#
+      .SYNOPSIS
+      This function get a Opportunity through the Autotask Web Services API.
+      .DESCRIPTION
+      This function get a Opportunity through the Autotask Web Services API.
+      .EXAMPLE
+      Get-AtwsOpportunity [-ParameterName] [Parameter value]
+      Use Get-Help Get-AtwsOpportunity
+      .NOTES
+      NAME: Get-AtwsOpportunity
+  #>
+	  [CmdLetBinding(DefaultParameterSetName='Filter')]
+    Param
+    (
+                [Parameter(
+          Mandatory = $true,
+          ValueFromRemainingArguments = $true,
+          ParameterSetName = 'Filter')]
+        [ValidateNotNullOrEmpty()]
+        [String[]]
+        $Filter ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [long]
+         $id
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [Int]
+         $AccountID
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $AdvancedField1
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $AdvancedField2
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $AdvancedField3
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $AdvancedField4
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $AdvancedField5
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $Amount
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [string]
+         $Barriers
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [Int]
+         $ContactID
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $Cost
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [datetime]
+         $CreateDate
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [string]
+         $HelpNeeded
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [Int]
+         $LeadReferral
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [string]
+         $Market
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [string]
+         $NextStep
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [Int]
+         $OwnerResourceID
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [Int]
+         $ProductID
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [datetime]
+         $ProjectedCloseDate
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [datetime]
+         $ProjectedLiveDate
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [string]
+         $PromotionName
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [ValidateSet('Days','Months','Years')]
+
+        [String]
+         $RevenueSpreadUnit
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [ValidateSet('New Lead','Stage 1 First Contact','Stage 2 Qualification','Stage 3 Proposal Sent','Stage 4 Contract Sent','Stage 5 Closed')]
+
+        [String]
+         $Stage
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [ValidateSet('Not Ready To Buy','Active','Lost','Closed','Implemented')]
+
+        [String]
+         $Status
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [datetime]
+         $ThroughDate
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [string]
+         $Title
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [ValidateSet('Hot','Warm','Cold')]
+
+        [String]
+         $Rating
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [datetime]
+         $ClosedDate
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [float]
+         $AssessmentScore
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [float]
+         $TechnicalAssessmentScore
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [float]
+         $RelationshipAssessmentScore
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [Int]
+         $PrimaryCompetitor
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [ValidateSet('Love the product','See the value','Couple of features we really like','Like our company','Trust our reputation')]
+
+        [String]
+         $WinReason
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [ValidateSet("Don’t need our product",'Not a good time','Too expensive',"Competitor’s version offers more",'Couple of features we really need','Not remotely interested')]
+
+        [String]
+         $LossReason
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [string]
+         $WinReasonDetail
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [string]
+         $LossReasonDetail
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [datetime]
+         $LastActivity
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [datetime]
+         $DateStamp
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [Int]
+         $Probability
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [Int]
+         $RevenueSpread
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [boolean]
+         $UseQuoteTotals
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [Int]
+         $TotalAmountMonths
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [Int]
+         $SalesProcessPercentComplete
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [Int]
+         $SalesOrderID
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $OnetimeCost
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $OnetimeRevenue
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $MonthlyCost
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $MonthlyRevenue
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $QuarterlyCost
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $QuarterlyRevenue
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $SemiannualCost
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $SemiannualRevenue
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $YearlyCost
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $YearlyRevenue
+
+    )
+
+
+
+          
+
+  Begin
+  { 
+    If (-not($global:atws.Url))
+    {
+      Throw [ApplicationException] 'Not connected to Autotask WebAPI. Run Connect-AutotaskWebAPI first.'
+    }
+    Write-Verbose ('{0}: Begin of function' -F $MyInvocation.MyCommand.Name)
+
+  }   
+
+  Process
+  {     
+
+    If (-not($Filter))
+    {
+        $Fields = $Atws.GetFieldInfo('Opportunity')
+        
+        Foreach ($Parameter in $PSBoundParameters.GetEnumerator())
+        {
+            $Field = $Fields | Where-Object {$_.Name -eq $Parameter.Key}
+            If ($Field.IsPickList)
+            {
+              $PickListValue = $Field.PickListValues | Where-Object {$_.Label -eq $Parameter.Value}
+              $Value = $PickListValue.Value
+            }
+            Else
+            {
+              $Value = $Parameter.Value
+            }
+            $Filter += $Parameter.Key
+            $Filter += '-eq'
+            $Filter += $Value
+        }
+        
+    }
+
+    Get-AtwsData -Entity Opportunity -Filter $Filter }   
+
+  End
+  {
+    Write-Verbose ('{0}: End of function' -F $MyInvocation.MyCommand.Name)
+
+  }
+
+
+        
+}
---
+++ 0.2.0.0/Public/New-AtwsOpportunity.ps1
@@ -0,0 +1,460 @@
+﻿<#
+
+.COPYRIGHT
+Copyright (c) Office Center Hønefoss AS. All rights reserved. Based on code from Jan Egil Ring (Crayon). Licensed under the MIT license.
+See https://github.com/officecenter/OCH-Public/blob/master/LICENSE for license information.
+
+#>
+
+Function New-AtwsOpportunity
+{
+  <#
+      .SYNOPSIS
+      This function creates a new Opportunity through the Autotask Web Services API.
+      .DESCRIPTION
+      This function creates a new Opportunity through the Autotask Web Services API.
+      .EXAMPLE
+      New-AtwsOpportunity [-ParameterName] [Parameter value]
+      Use Get-Help New-AtwsOpportunity
+      .NOTES
+      NAME: New-AtwsOpportunity
+  #>
+	  [CmdLetBinding(DefaultParameterSetName='By_parameters')]
+    Param
+    (
+                [Parameter(
+          Mandatory = $true,
+          ValueFromRemainingArguments = $true,
+          ParameterSetName = 'By_parameters')]
+        [ValidateNotNullOrEmpty()]
+        [Int]
+        $Id ,
+
+        [Parameter(
+          Mandatory = $True,
+          ParameterSetName = 'By_parameters'
+        )]
+         [ValidateNotNullOrEmpty()]
+         [Int]
+         $AccountID
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $AdvancedField1
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $AdvancedField2
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $AdvancedField3
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $AdvancedField4
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $AdvancedField5
+ ,
+
+        [Parameter(
+          Mandatory = $True,
+          ParameterSetName = 'By_parameters'
+        )]
+         [ValidateNotNullOrEmpty()]
+         [decimal]
+         $Amount
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [string]
+         $Barriers
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [Int]
+         $ContactID
+ ,
+
+        [Parameter(
+          Mandatory = $True,
+          ParameterSetName = 'By_parameters'
+        )]
+         [ValidateNotNullOrEmpty()]
+         [decimal]
+         $Cost
+ ,
+
+        [Parameter(
+          Mandatory = $True,
+          ParameterSetName = 'By_parameters'
+        )]
+         [ValidateNotNullOrEmpty()]
+         [datetime]
+         $CreateDate
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [string]
+         $HelpNeeded
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [Int]
+         $LeadReferral
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [string]
+         $Market
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [string]
+         $NextStep
+ ,
+
+        [Parameter(
+          Mandatory = $True,
+          ParameterSetName = 'By_parameters'
+        )]
+         [ValidateNotNullOrEmpty()]
+         [Int]
+         $OwnerResourceID
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [Int]
+         $ProductID
+ ,
+
+        [Parameter(
+          Mandatory = $True,
+          ParameterSetName = 'By_parameters'
+        )]
+         [ValidateNotNullOrEmpty()]
+         [datetime]
+         $ProjectedCloseDate
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [datetime]
+         $ProjectedLiveDate
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [string]
+         $PromotionName
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [ValidateSet('Days','Months','Years')]
+
+        [String]
+         $RevenueSpreadUnit
+ ,
+
+        [Parameter(
+          Mandatory = $True,
+          ParameterSetName = 'By_parameters'
+        )]
+         [ValidateNotNullOrEmpty()]
+         [ValidateSet('New Lead','Stage 1 First Contact','Stage 2 Qualification','Stage 3 Proposal Sent','Stage 4 Contract Sent','Stage 5 Closed')]
+
+        [String]
+         $Stage
+ ,
+
+        [Parameter(
+          Mandatory = $True,
+          ParameterSetName = 'By_parameters'
+        )]
+         [ValidateNotNullOrEmpty()]
+         [ValidateSet('Not Ready To Buy','Active','Lost','Closed','Implemented')]
+
+        [String]
+         $Status
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [datetime]
+         $ThroughDate
+ ,
+
+        [Parameter(
+          Mandatory = $True,
+          ParameterSetName = 'By_parameters'
+        )]
+         [ValidateNotNullOrEmpty()]
+         [string]
+         $Title
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [ValidateSet('Hot','Warm','Cold')]
+
+        [String]
+         $Rating
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [datetime]
+         $ClosedDate
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [Int]
+         $PrimaryCompetitor
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [ValidateSet('Love the product','See the value','Couple of features we really like','Like our company','Trust our reputation')]
+
+        [String]
+         $WinReason
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [ValidateSet("Don’t need our product",'Not a good time','Too expensive',"Competitor’s version offers more",'Couple of features we really need','Not remotely interested')]
+
+        [String]
+         $LossReason
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [string]
+         $WinReasonDetail
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [string]
+         $LossReasonDetail
+ ,
+
+        [Parameter(
+          Mandatory = $True,
+          ParameterSetName = 'By_parameters'
+        )]
+         [ValidateNotNullOrEmpty()]
+         [Int]
+         $Probability
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [Int]
+         $RevenueSpread
+ ,
+
+        [Parameter(
+          Mandatory = $True,
+          ParameterSetName = 'By_parameters'
+        )]
+         [ValidateNotNullOrEmpty()]
+         [boolean]
+         $UseQuoteTotals
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [Int]
+         $TotalAmountMonths
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $OnetimeCost
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $OnetimeRevenue
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $MonthlyCost
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $MonthlyRevenue
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $QuarterlyCost
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $QuarterlyRevenue
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $SemiannualCost
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $SemiannualRevenue
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $YearlyCost
+ ,
+
+        [Parameter(
+          Mandatory = $False,
+          ParameterSetName = 'By_parameters'
+        )]
+         [decimal]
+         $YearlyRevenue
+
+    )
+
+
+
+          
+
+  Begin
+  { 
+    If (-not($global:atws.Url))
+    {
+      Throw [ApplicationException] 'Not connected to Autotask WebAPI. Run Connect-AutotaskWebAPI first.'
+    }
+    Write-Verbose ('{0}: Begin of function' -F $MyInvocation.MyCommand.Name)
+
+  }   
+
+  Process
+  {   
+
+    $InputObject = New-Object Autotask.Opportunity
+
+    Foreach ($Parameter in $PSBoundParameters.GetEnumerator())
+    {
+        $InputObject.$($Parameter.Key) = $Parameter.Value
+    }
+
+    New-AtwsData -Entity $InputObject
+ }   
+
+  End
+  {
+    Write-Verbose ('{0}: End of function' -F $MyInvocation.MyCommand.Name)
+
+  }
+
+
+        
+}
