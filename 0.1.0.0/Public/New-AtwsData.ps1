78030df86bd01964f5b1e003d794bb5c0bf9f020
Hugo Klemmestad <hugo@office-center.no>
2017-12-06 08:08:45 +0100

    Updated new function

--- 0.1.0.0/Public/New-AtwsData.ps1
+++ 0.1.0.0/Public/New-AtwsData.ps1
@@ -15,10 +15,38 @@ Function New-AtwsData
         Mandatory = $True,
         Position = 0
     )]
+    [Autotask.Entity]
     $Entity
   )
    
-  Write-Verbose ('{0}: Creating a new object of type Autotask.{1}' -F $MyInvocation.MyCommand.Name, $Entity) 
-  New-Object -TypeName Autotask.$Entity
+  Begin
+  { 
+    If (-not($global:atws.Url))
+    {
+        
+      Throw [ApplicationException] 'Not connected to Autotask WebAPI. Run Connect-AutotaskWebAPI first.'
+    }
+  }
+    
+  Process
+  { 
+    Write-Verbose ('{0}: Creating a new object of type Autotask.{1}' -F $MyInvocation.MyCommand.Name, $Entity) 
+    $Result = $Atws.Create($Entity)
 
+    If ($Result.Errors.Count -eq 0)
+    {
+      Return $Result
+    }
+    Else
+    {
+      Foreach ($AtwsError in $Result.Errors)
+      {
+        Write-Error $AtwsError.Message
+      }
+    }
+  }
+  End 
+  {
+    Write-Verbose ('{0}: End of function' -F $MyInvocation.MyCommand.Name) 
+  }
 }
