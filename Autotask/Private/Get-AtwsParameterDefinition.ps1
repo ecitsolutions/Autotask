ca991a2e2c202fb97d23cd14c30d8ece984ad561
Hugo Klemmestad <hugo@office-center.no>
2018-01-30 13:03:21 +0100

    set parameters to $null

--- Autotask/Private/Get-AtwsParameterDefinition.ps1
+++ Autotask/Private/Get-AtwsParameterDefinition.ps1
@@ -132,7 +132,7 @@
       $ParameterOptions = @{
         Mandatory              = $Field.Mandatory
         ParameterSetName       = $Field.ParameterSet
-        ValidateNotNullOrEmpty = $True
+        #ValidateNotNullOrEmpty = $True
         ValidateLength         = $Field.Length
         ValidateSet            = $Field.PickListValues.Label | Select-Object -Unique
         Array                  = $(($Verb -eq 'Get'))
