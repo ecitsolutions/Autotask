547a8cfdd5b46e24d6dfa7d69e3bf7c992c70937
Hugo Klemmestad <hugo@office-center.no>
2018-04-26 08:53:34 +0200

    Test-AutotaskApiUser

---
+++ Scripts/Test-AutotaskApiUser.ps1
@@ -0,0 +1,57 @@
+﻿
+#$Credentials = Get-Credential
+$User = "username@domain"
+$PWord = ConvertTo-SecureString -String "password" -AsPlainText -Force
+$Credentials = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $User, $PWord
+
+
+# Modify for correct services URI
+$atws = New-WebServiceProxy -URI https://webservices4.Autotask.net/atservices/1.5/atws.wsdl -Credential $credentials
+
+$QueryXML = @"
+<queryxml>
+  <entity>Account</entity>
+  <query>
+    <field>id
+      <expression op="Equals">0</expression>
+    </field>
+  </query>
+</queryxml>
+"@
+
+$Result = $Atws.query($QueryXML)
+
+If ($Result.ReturnCode -eq 1) {
+  Write-Output "Account query returned no errors, connection seems fine."
+}
+
+Foreach ($Err in $Result.Errors) {
+  Write-Output $Err.message
+}
+
+# Get username part of credential
+$UserName = $Credentials.UserName.Split('@')[0].Trim('\')
+
+Write-Output "Username: $Username "
+
+$QueryXML = @"
+<queryxml>
+  <entity>Resource</entity>
+  <query>
+    <field>username
+      <expression op="Equals">$UserName</expression>
+    </field>
+  </query>
+</queryxml>
+"@
+
+$Result = $Atws.query($QueryXML)
+
+If ($Result.ReturnCode -eq 1) {
+  Write-Output "Username query returned no errors, connection seems fine."
+}
+
+Foreach ($Err in $Result.Errors) {
+  Write-Output $Err.message
+}
+
