51185c2a1487d3e023318699f7d6ceae503605b7
Hugo Klemmestad <hugo@office-center.no>
2018-04-27 14:17:51 +0200

    TLS 1.2

--- Scripts/Test-AutotaskApiUser.ps1
+++ Scripts/Test-AutotaskApiUser.ps1
@@ -4,6 +4,11 @@ $User = "username@domain"
 $PWord = ConvertTo-SecureString -String "password" -AsPlainText -Force
 $Credentials = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $User, $PWord
 
+# Add .NET support for TLS 1.2 if missing
+$Protocol = [System.Net.ServicePointManager]::SecurityProtocol
+If ($Protocol.ToString() -notlike '*Tls12*') { 
+  [System.Net.ServicePointManager]::SecurityProtocol += 'tls12'
+}
 
 # Modify for correct services URI
 $atws = New-WebServiceProxy -URI https://webservices4.Autotask.net/atservices/1.5/atws.wsdl -Credential $credentials
