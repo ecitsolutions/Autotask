9cb1a97b0fce37f4ad26a40a61d73a1dd3e3635f
hugo <hugo@office-center.no>
2019-01-07 18:17:22 +0100

    New custom function to get detailed invoice information based on invoice ID

---
+++ Autotask/Private/Get-AtwsInvoiceInfo.ps1
@@ -0,0 +1,76 @@
+<#
+
+    .COPYRIGHT
+    Copyright (c) Office Center Hønefoss AS. All rights reserved. Licensed under the MIT license.
+    See https://github.com/officecenter/Autotask/blob/master/LICENSE.md  for license information.
+
+#>
+
+Function Get-AtwsInvoiceInfo {
+  <#
+      .SYNOPSIS
+      This function collects information about a specific Autotask invoice object and returns a generic
+      powershell object with all relevant information as a starting point for import into other systems.
+      .DESCRIPTION
+      The function accepts an invoice object or an invoice id and makes a special API call to get a 
+      complete invoice description, including billingitems. For some types of billingitems additional
+      information may be collected. All information is collected and stored in a PSObject which is
+      returned.
+      .INPUTS
+      An Autotask invoice object or an invoice id
+      .OUTPUTS
+      A custom PSObject with detailed information about an invoice
+      .EXAMPLE
+      $Invoice | Get-#PrefixInvoiceInfo
+      Gets information about invoices passed through the pipeline
+      .EXAMPLE
+      Get-#PrefixInvoiceInfo -InvoiceID $Invoice.id
+      Gets information about invoices based on the ids passed as a parameter
+      .NOTES
+      NAME: Get-#PrefixInvoiceInfo
+      
+  #>
+	
+  [cmdletbinding()]
+  Param
+  (
+    [Parameter(
+        Mandatory = $True,
+        ValueFromPipeLine = $True,
+        ParameterSetName = 'Input_Object'
+    )]
+    [Autotask.Invoice[]]
+    $InputObject,
+
+    [Parameter(
+        Mandatory = $True,
+        ParameterSetName = 'By_parameters'
+    )]
+    [String[]]
+    $InvoiceId
+  )
+  
+  Begin {
+  
+    $Prefix = '#Prefix'
+    
+    If ($PSCmdlet.ParameterSetName -eq 'Input_Object') {
+      $InvoiceId = $InputObject.id
+    }
+
+  }
+
+  Process {
+  
+    $Result = @()
+  
+    ForEach ($Id in $InvoiceId) {
+      [Xml]$InvoiceInfo = $AtwsConnection[$Prefix].GetInvoiceMarkup($Id,'XML')
+      $Result += $InvoiceInfo.invoice_batch_generic | ConvertTo-PSObject
+    }
+  }
+
+  End {
+    Return $Result
+  }
+}
