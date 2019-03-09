626cd0383527d02579ebc0e9d425ddeefa55dade
hugo <hugo@office-center.no>
2019-03-09 16:20:16 +0100

    Improved Get-FieldInfo to return static and dynamic entities

--- Autotask16/Public/Get-FieldInfo.ps1
+++ Autotask16/Public/Get-FieldInfo.ps1
@@ -23,7 +23,8 @@ Function Get-FieldInfo {
 	
   [cmdletbinding(
       SupportsShouldProcess = $True,
-      ConfirmImpact = 'Low'
+      ConfirmImpact = 'Low',
+      DefaultParameterSetName = 'by_Entity'
   )]
   Param
   (
@@ -34,15 +35,43 @@ Function Get-FieldInfo {
     $All,
     
     [Parameter(
+        ParameterSetName = 'get_Static'
+    )]
+    [Switch]
+    $Static, 
+ 
+    [Parameter(
+        ParameterSetName = 'get_Dynamic'
+    )]
+    [Switch]
+    $Dynamic, 
+     
+    [Parameter(
+        Mandatory = $True,
+        ParameterSetName = 'by_Reference'
+    )]
+    [Switch]
+    $ReferencingEntity,  
+     
+    [Parameter(
+        ParameterSetName = 'by_Entity'
+    )]
+    [Alias('UDF')]
+    [Switch]
+    $UserDefinedFields, 
+       
+    [Parameter(
         Mandatory = $True,
         Position = 0,
         ParameterSetName = 'by_Entity'
     )]
+    [Parameter(
+        Mandatory = $True,
+        Position = 0,
+        ParameterSetName = 'by_Reference'
+    )]
     [String]
-    $Entity,
-    
-    [Int]
-    $ProgressParentId
+    $Entity
   )
     
   Begin { 
@@ -83,8 +112,92 @@ Function Get-FieldInfo {
   
   Process { 
         
-    If ($All.IsPresent) {
+    # By ENTITY
+    If ($PSCmdlet.ParameterSetName -eq 'by_Entity')
+    {
+      If (($script:FieldInfoCache[$Entity].HasPicklist -or $script:FieldInfoCache[$Entity].EntityInfo.HasUserDefinedFields) -and $script:FieldInfoCache[$Entity].RetrievalTime -lt $CacheExpiry) { 
+        $Caption = $MyInvocation.MyCommand.Name
+        $VerboseDescrition = '{0}: About to get built-in fields for {1}s' -F $Caption, $Entity
+        $VerboseWarning = '{0}: About to get built-in fields for {1}s. Do you want to continue?' -F $Caption, $Entity
+
+        If ($PSCmdlet.ShouldProcess($VerboseDescrition, $VerboseWarning, $Caption)) { 
+          $Result = $Script:atws.GetFieldInfo($Entity)
+          $UDF = $Script:atws.GetUDFInfo($Entity)
+        }
+   
     
+        If ($Result.Errors.Count -gt 0) {
+          Foreach ($AtwsError in $Result.Errors) {
+            Write-Error $AtwsError.Message
+          }
+          Return
+        }
+      
+        # No errors
+        # Test if value has changed
+        If (Compare-PSObject -ReferenceObject $script:FieldInfoCache[$Entity].FieldInfo -DifferenceObject $Result) { 
+     
+          # No errors
+          Write-Verbose ('{0}: Save or update FieldInfo cache for entity {1}' -F $MyInvocation.MyCommand.Name, $Entity)
+          $script:FieldInfoCache[$Entity].FieldInfo = $Result
+          
+          Write-Warning ('Entity {0} has been modified in Autotask! Re-import module with -Force to refresh.' -F $Entity)
+ 
+        }
+        
+        If ($script:FieldInfoCache[$Entity].EntityInfo.HasUserDefinedFields)
+        { 
+          If (Compare-PSObject -ReferenceObject $script:FieldInfoCache[$Entity].UDFInfo -DifferenceObject $UDF) { 
+     
+            # No errors
+            Write-Verbose ('{0}: Save or update UDF cache for entity {1}' -F $MyInvocation.MyCommand.Name, $Entity)
+            $script:FieldInfoCache[$Entity].UDFInfo = $UDF
+          
+            Write-Warning ('Entity {0} has been modified in Autotask! Re-import module with -Force to refresh.' -F $Entity)
+ 
+          }
+        }
+        $script:FieldInfoCache[$Entity].RetrievalTime = Get-Date
+      
+        # Mark chache as dirty == dump to disk
+        $CacheDirty = $True
+      }
+      Else {
+        # Prepare an empty result set. If none of the conditions below are true, then the user tried to get
+        # UDFs from an entity that does not support them. The result will be empty.
+        $Result = @()  
+        
+        # If the user asked for UDFs and the entity supports UDFs, return the info. 
+        If ($UserDefinedFields.IsPresent -and $script:FieldInfoCache[$Entity].EntityInfo.HasUserDefinedFields)
+        {
+          Write-Verbose ('{0}: Returning fieldinfo for entity {1} from cache' -F $MyInvocation.MyCommand.Name, $Entity)   
+          $Result = $script:FieldInfoCache[$Entity].UDFInfo
+        }
+        # If the user askt for fieldinfo (default), return fieldinfo
+        ElseIf ($PSCmdlet.ParameterSetName -eq 'by_Entity')
+        { 
+          Write-Verbose ('{0}: Returning fieldinfo for entity {1} from cache' -F $MyInvocation.MyCommand.Name, $Entity)   
+          $Result = $script:FieldInfoCache[$Entity].FieldInfo
+        }
+      }
+    }
+    # ReferencingEntity
+    ElseIf($PSCmdlet.ParameterSetName -eq 'by_Reference')
+    {
+      $Result = @()
+      Foreach ($Object in $Script:FieldInfoCache.GetEnumerator())
+      {
+        $IsReferencing = $Object.Value.FieldInfo.Where({$_.ReferenceEntityType -eq $Entity})
+        If($IsReferencing)
+        {
+          $Result += $Object.Name
+        }
+      }
+    }
+    # For all other options, check the cache version
+    Else 
+    { 
+  
       # If the API version has been changed at the Autotask end we unfortunately have to load all
       # entities from scratch
       $CurrentApiVersion = $Script:Atws.GetWsdlVersion()
@@ -94,11 +207,6 @@ Function Get-FieldInfo {
         $ProgressParameters = @{
           Activity = ('New API version {0} has been published, old version is {1}. recreating diskcache.' -F $CurrentApiVersion, $Script:Cache[$Script:Atws.CI].ApiVersion)
           Id = 9
-        }
-
-        # Add parentid if supplied
-        If ($ProgressParentId) {
-          $ProgressParameters['ParentId'] = $ProgressParentId
         }
                  
         $script:FieldInfoCache = @{}
@@ -129,12 +237,19 @@ Function Get-FieldInfo {
             If ($FieldInfo.Where({$_.IsPicklist})) {
               $HasPickList = $True
             }
-            
+                       
             # Create Cache entry
             $CacheEntry = New-Object -TypeName PSObject -Property @{
               HasPickList   = $HasPickList
               RetrievalTime = Get-Date
             }
+            
+            # Check if entity has userdefined fields
+            If ($Object.HasUserDefinedFields) {
+              $UDF = $Script:Atws.GetUDFInfo($Object.Name)
+              Add-Member -InputObject $CacheEntry -MemberType NoteProperty -Name UDFInfo -Value $UDF -Force
+            }
+                        
             # Add complext objects as properties
             Add-Member -InputObject $CacheEntry -MemberType NoteProperty -Name EntityInfo -Value $Object -Force
             Add-Member -InputObject $CacheEntry -MemberType NoteProperty -Name FieldInfo -Value $FieldInfo -Force
@@ -145,27 +260,43 @@ Function Get-FieldInfo {
         
         # Add cache to $Cache object and save to disk
         $Script:Cache[$Script:Atws.CI].FieldInfoCache = $FieldInfoCache 
+        $Script:Cache[$Script:Atws.CI].ApiVersion = $CurrentApiVersion
+        $Script:Cache['00'].ApiVersion = $CurrentApiVersion
+        $Base = $FieldInfoCache.Clone()
+        
+        # Clean Instance specific info from Base
+        Foreach ($Object in $Base.GetEnumerator().Where({$_.Value.HasPickList -or $_.Value.EntityInfo.HasUserDefinedFields}))
+        {
+          Foreach ($PickList in $Object.FieldInfo.Where({$_.IsPickList}))
+          {
+            $PickList.PicklistValues = $Null
+          }
+          
+          If ($Object.EntityInfo.HasUserDefinedFields)
+          {
+            $Object.UserDefinedFields = $Null
+          }
+        }
+        
+        $Script:Cache['00'].FieldInfoCache = $Base
         
         # Mark chache as dirty == dump to disk
         $CacheDirty = $True
         
+        # Write-Warning to inform user that an update of static functions is due
+        Write-Warning ('{0}: API version has been updated. You need to run Update-AtwsStaticFunctions with writepermissions to the module directory or update the module.' -F $MyInvocation.MyCommand.Name) 
+        
       }
-      # At this point we have a cache, but we do not know if it is fresh. We only need to check
-      # entities with picklists
-      Else { 
-
+      # API version is the same, do we need to check picklists?
+      ElseIf (@('get_All', 'get_Dynamic') -contains $PSCmdlet.ParameterSetName)
+      { 
         # Prepare parameters for @splatting
         $ProgressParameters = @{
           Activity = 'All entities has been requested. Updating picklists.'
           Id = 9
         }
-
-        # Add parentid if supplied
-        If ($ProgressParentId) {
-          $ProgressParameters['ParentId'] = $ProgressParentId
-        }
       
-        $Entities = $script:FieldInfoCache.GetEnumerator().Where{$_.Value.HasPicklist}
+        $Entities = $script:FieldInfoCache.GetEnumerator().Where{$_.Value.HasPicklist -or $_.Value.EntityInfo.HasUserDefinedfields}
       
         Foreach ($Object in $Entities) {
       
@@ -180,7 +311,7 @@ Function Get-FieldInfo {
           Write-Progress -Status $Status -PercentComplete $PercentComplete -CurrentOperation $CurrentOperation @ProgressParameters
         
           # Is the Cache too old? I.E. older than 15 minutes?
-          If($Object.RetrievalTime -lt $CacheExpiry) {
+          If($Object.Value.RetrievalTime -lt $CacheExpiry) {
           
             # Force a refresh by calling this function
             $null = Get-FieldInfo -Entity $Object.Key
@@ -188,45 +319,25 @@ Function Get-FieldInfo {
           }
         }
       }
-      # Return the current FieldInfoCache
-      $Result = $script:FieldInfoCache
-      
-    }
-    ElseIf ($script:FieldInfoCache[$Entity].HasPicklist -and $script:FieldInfoCache[$Entity].RetrievalTime -lt $CacheExpiry) { 
-      $Caption = $MyInvocation.MyCommand.Name
-      $VerboseDescrition = '{0}: About to get built-in fields for {1}s' -F $Caption, $Entity
-      $VerboseWarning = '{0}: About to get built-in fields for {1}s. Do you want to continue?' -F $Caption, $Entity
-
-      If ($PSCmdlet.ShouldProcess($VerboseDescrition, $VerboseWarning, $Caption)) { 
-        $Result = $Script:atws.GetFieldInfo($Entity)
-      }
-   
     
-      If ($Result.Errors.Count -gt 0) {
-        Foreach ($AtwsError in $Result.Errors) {
-          Write-Error $AtwsError.Message
+
+      # Cache has been loaded, has the right API version and everything is up to date
+      # Return the correct set
+      $Result = Switch ($PSCmdLet.ParameterSetName) 
+      { 
+        'get_All' 
+        {
+          $script:FieldInfoCache
         }
-        Return
-      }
-      
-      # No errors
-      # Test if value has changed
-      If (Compare-PSObject -ReferenceObject $script:FieldInfoCache[$Entity].FieldInfo -DifferenceObject $Result) { 
-     
-        # No errors
-        Write-Verbose ('{0}: Save or update cache for entity {1}' -F $MyInvocation.MyCommand.Name, $Entity)
-        $script:FieldInfoCache[$Entity].FieldInfo = $Result
-        $script:FieldInfoCache[$Entity].RetrievalTime = Get-Date
-      
-        # Mark chache as dirty == dump to disk
-        $CacheDirty = $True
-      }
-           
-    }
-    # Not -All and Entity either does not have any picklists or picklist has been requested within the last 15 minutes
-    Else {
-      Write-Verbose ('{0}: Returning fieldinfo for entity {1} from cache' -F $MyInvocation.MyCommand.Name, $Entity)      
-      $Result = $script:FieldInfoCache[$Entity].FieldInfo
+        'get_Static'
+        {
+          $script:FieldInfoCache.GetEnumerator() | Where-Object {-not $_.Value.HasPickList -and -not $_.Value.EntityInfo.HasUserDefinedFields}
+        }
+        'get_Dynamic'
+        {
+          $script:FieldInfoCache.GetEnumerator() | Where-Object {$_.Value.HasPickList -or $_.Value.EntityInfo.HasUserDefinedFields}
+        }
+      } 
     }
   }  
   End {
