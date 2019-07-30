d7098218ff927091630f5e16b95114712e1acec2
hugo <hugo@office-center.no>
2019-07-30 09:47:54 +0200

    Prettify code

--- Tests/Autotask.Module.Issues.Tests.ps1
+++ Tests/Autotask.Module.Issues.Tests.ps1
@@ -1,379 +1,245 @@
 ﻿<#
 
-    .COPYRIGHT
-    Copyright (c) Office Center Hønefoss AS. All rights reserved. Based on code from Jan Egil Ring (Crayon). Licensed under the MIT license.
-    See https://github.com/officecenter/Autotask/blob/master/LICENSE.md for license information.
+        .COPYRIGHT
+        Copyright (c) Office Center Hønefoss AS. All rights reserved. Based on code from Jan Egil Ring (Crayon). Licensed under the MIT license.
+        See https://github.com/officecenter/Autotask/blob/master/LICENSE.md for license information.
 
-    .SYNOPSIS
-    This set of tests has a test per issue reported on GitHub and is supposed to guard against re-introducing old bugs
-    .DESCRIPTION
-    In this Pester script I have written one or a set of tests per issue reported on GitHub. This is will hopefully discover any mistakes that re-introduces old bugs.
+        .SYNOPSIS
+        This set of tests has a test per issue reported on GitHub and is supposed to guard against re-introducing old bugs
+        .DESCRIPTION
+        In this Pester script I have written one or a set of tests per issue reported on GitHub. This is will hopefully discover any mistakes that re-introduces old bugs.
 
-    .INPUTS
-    Nothing. This function only takes parameters.
-    .OUTPUTS
-    Nothing.
+        .INPUTS
+        Nothing. This function only takes parameters.
+        .OUTPUTS
+        Nothing.
 
 #>
 
 [cmdletbinding(
-  SupportsShouldProcess = $True,
-  ConfirmImpact = 'Low',
-  DefaultParameterSetName = 'Default'
+        SupportsShouldProcess = $True,
+        ConfirmImpact = 'Low',
+        DefaultParameterSetName = 'Default'
 )]
 
 Param
 (
-  [Parameter(
-    Mandatory = $true,
-    ParameterSetName = 'Default'
-  )]
-  [ValidateNotNullOrEmpty()]    
-  [pscredential]
-  $Credential,
+    [Parameter(
+            Mandatory = $True,
+            ParameterSetName = 'Default'
+    )]
+    [ValidateNotNullOrEmpty()]    
+    [pscredential]
+    $Credential,
     
-  [Parameter(
-    Mandatory = $true,
-    ParameterSetName = 'Default'
-  )]
-  [String]
-  $ApiTrackingIdentifier
+    [Parameter(
+            Mandatory = $True,
+            ParameterSetName = 'Default'
+    )]
+    [String]
+    $ApiTrackingIdentifier
 )
 
 # Information about module location
 $ModuleName = 'Autotask'
 
-$ModulePath = '{0}\{1}' -F $(Split-Path -Parent (Split-Path -Parent $MyInvocation.MyCommand.Path)), $ModuleName
+$ModulePath = '{0}\{1}' -F $(Split-Path -Parent -Path (Split-Path -Parent -Path $MyInvocation.MyCommand.Path)), $ModuleName
 
 Describe 'Issue #44' -Tag 'Issues' {
+    Import-Module $ModulePath -Force -ArgumentList $Credential, $ApiTrackingIdentifier
 
-  Context 'Issue #44: GetEntityByReferenceId documentation ' {
-    
-    $Contract = Get-AtwsContract -AccountID 0 -IsDefaultContract $True
-    $Account = Get-AtwsContract -id $Contract.Id -GetReferenceEntityById AccountID
+    Context 'Issue #44: GetEntityByReferenceId documentation ' {
+        $Contract = Get-AtwsContract -AccountID 0 -IsDefaultContract $True
+        $Account = Get-AtwsContract -id $Contract.Id -GetReferenceEntityById AccountID
 
-    It 'Account 0 should have a default contract' {
-      $Contract.Count | Should -Be 1
+        It 'Account 0 should have a default contract' {$Contract.Count | Should -Be 1}
+
+        It '$Contract should be a contract' {$Contract | Should -BeOfType Autotask.Contract}
+
+        It '-GetReferenceEntityById AccountID should return a single account' {$Account.Count | Should -Be 1}
+
+        It 'should be an Account and have id 0' {
+            $Account | Should -BeOfType Autotask.Account
+            $Account.id | Should -Be 0
+        }
     }
-
-    It '$Contract should be a contract' {
-      $Contract | Should -BeOfType Autotask.Contract
-    }
-
-    It '-GetReferenceEntityById AccountID should return a single account' {
-      $Account.Count | Should -Be 1
-    }
-
-    It 'should be an Account and have id 0' {
-      $Account | Should -BeOfType Autotask.Account
-      $Account.id | Should -Be 0
-    }
-
-  }
-
 }
 
 Describe 'Issue #43' -Tag 'Issues' {
-
-  Context 'Issue #43: New-AtwsAttachment adds timezone difference twice ' { 
-    
     Import-Module $ModulePath -Force -ArgumentList $Credential, $ApiTrackingIdentifier
 
-    InModuleScope Autotask { 
-      
-      # Get a datetime object
-      $CreateDate = Get-Date
-      $Atws = Get-AtwsConnectionObject -Confirm:$false
+    Context 'Issue #43: New-AtwsAttachment adds timezone difference twice ' {
+        InModuleScope Autotask {
+            # Get a datetime object
+            $CreateDate = Get-Date
+            $Atws = Get-AtwsConnectionObject -Confirm:$false
 
-      Mock 'Get-AtwsAttachmentInfo' {
-        [PSCustomObject]@{
-          PSTypeName = 'Autotask.AttachmentInfo'
-          CreateDate = $CreateDate
+            Mock 'Get-AtwsAttachmentInfo' {
+                [PSCustomObject]@{
+                    PSTypeName = 'Autotask.AttachmentInfo'
+                    CreateDate = $CreateDate
+                }
+            }
+
+            Mock 'Get-AtwsTicket' {Return $True}
+
+            # Mock CreateAttachment()
+            $CreateAttachmentMethod = @{
+                Type  = 'ScriptMethod'
+                Name  = 'CreateAttachment'
+                Value = { 1234 }
+                Force = $True
+            }
+            $Atws | Add-Member @CreateAttachmentMethod 
+
+            $Result = New-AtwsAttachment -Uri https://google.com -TicketId 0
+
+            It 'should call with mocked AttachmentId' {
+                $AssertParams = @{
+                    CommandName     = 'Get-AtwsAttachmentInfo'
+                    ParameterFilter = { $id -eq 1234 }
+                }
+                Assert-MockCalled @AssertParams
+            }
+
+            It 'should return the date unchanged' {$Result.CreateDate | Should -Be $CreateDate}
         }
-      }
-
-      Mock 'Get-AtwsTicket' {
-        Return $True
-      }
-
-      # Mock CreateAttachment()
-      $CreateAttachmentMethod = @{
-        Type  = 'ScriptMethod'
-        Name  = 'CreateAttachment'
-        Value = { 1234 }
-        Force = $True
-      }
-      $Atws | Add-Member @CreateAttachmentMethod 
-
-      $Result = New-AtwsAttachment -Uri https://google.com -TicketId 0
-
-      It 'should call with mocked AttachmentId' {
-        $AssertParams = @{
-          CommandName     = 'Get-AtwsAttachmentInfo'
-          ParameterFilter = { $id -eq 1234 }
-        }
-        Assert-MockCalled @AssertParams
-      }
-
-      It 'should return the date unchanged' {
-        $Result.CreateDate | Should -Be $CreateDate
-      }
     }
 
-  }
-
-  # Issue #42 is tested in Autotask.Module.Import.Tests.ps1
-
+    # Issue #42 is tested in Autotask.Module.Import.Tests.ps1
 }
 
 Describe 'Issue #41' -Tag 'Issues' {
+    Context 'Issue #41: Beta-module overwrites personal disk cache for release module ' { }
 
-  Context 'Issue #41: Beta-module overwrites personal disk cache for release module ' { }
+    # Issue #40 is an information request
 
-  # Issue #40 is an information request
-
-  # Issue #39 was retracted
-
+    # Issue #39 was retracted
 }
 
 Describe 'Issue #38' -Tag 'Issues' {
+    Context 'Issue #38: Feature request: Make connection object available to advanced users duplicate enhancement ' {
+        # Load the module in this context, a straight load with -Argumentlist is appropriate
+        $ModuleName = 'Autotask'
 
-  Context 'Issue #38: Feature request: Make connection object available to advanced users duplicate enhancement ' {
-
-    # Load the module in this context, a straight load with -Argumentlist is appropriate
-    $ModuleName = 'Autotask'
-
-    $ModulePath = '{0}\{1}' -F $(Split-Path -Parent (Split-Path -Parent $MyInvocation.MyCommand.Path)), $ModuleName
+        $ModulePath = '{0}\{1}' -F $(Split-Path -Parent -Path (Split-Path -Parent $MyInvocation.MyCommand.Path)), $ModuleName
     
-    Import-Module $ModulePath -Force -ArgumentList $Credential, $ApiTrackingIdentifier
+        Import-Module $ModulePath -Force -ArgumentList $Credential, $ApiTrackingIdentifier
 
-    $LoadedModule = Get-Module $ModuleName
+        $LoadedModule = Get-Module $ModuleName
 
-    It 'should be loaded' {
-      $LoadedModule.Name | Should -Be $ModuleName
+        It 'should be loaded' {$LoadedModule.Name | Should -Be $ModuleName}
+
+        It 'should export Get-AtwsConnectionObject' {$LoadedModule.ExportedCommands['Get-AtwsConnectionObject'].Name | Should -Be 'Get-AtwsConnectionObject'}
+
+        $Result = Get-AtwsConnectionObject -Confirm:$false
+
+        It 'should return an Autotask web proxy object' {$Result | Should -BeOfType Autotask.ATWS}
     }
-
-    It 'should export Get-AtwsConnectionObject' {
-      $LoadedModule.ExportedCommands['Get-AtwsConnectionObject'].Name | Should -Be 'Get-AtwsConnectionObject'
-    }
-
-    $Result = Get-AtwsConnectionObject -Confirm:$false
-
-    It 'should return an Autotask web proxy object' {
-      $Result | Should -BeOfType Autotask.ATWS
-    }
-
-  }
-
 }
 
 Describe 'Issue #37' -Tag 'Issues' {
+    Context 'Issue #37: Feature request: Attachments upload enhancement good first issue ' {
+        # Load the module in this context, a straight load with -Argumentlist is appropriate
+        $ModuleName = 'Autotask'
 
-  Context 'Issue #37: Feature request: Attachments upload enhancement good first issue ' {
+        $ModulePath = '{0}\{1}' -F $(Split-Path -Parent -Path (Split-Path -Parent $MyInvocation.MyCommand.Path)), $ModuleName
     
-    # Load the module in this context, a straight load with -Argumentlist is appropriate
-    $ModuleName = 'Autotask'
+        Import-Module $ModulePath -Force -ArgumentList $Credential, $ApiTrackingIdentifier
 
-    $ModulePath = '{0}\{1}' -F $(Split-Path -Parent (Split-Path -Parent $MyInvocation.MyCommand.Path)), $ModuleName
-    
-    Import-Module $ModulePath -Force -ArgumentList $Credential, $ApiTrackingIdentifier
+        $LoadedModule = Get-Module $ModuleName
 
-    $LoadedModule = Get-Module $ModuleName
+        It 'should be loaded' {$LoadedModule.Name | Should -Be $ModuleName}
 
-    It 'should be loaded' {
-      $LoadedModule.Name | Should -Be $ModuleName
+        It 'should export Get-AtwsAttachment' {$LoadedModule.ExportedCommands['Get-AtwsAttachment'].Name | Should -Be 'Get-AtwsAttachment'}
+
+        It 'should export New-AtwsAttachment' {$LoadedModule.ExportedCommands['New-AtwsAttachment'].Name | Should -Be 'New-AtwsAttachment'}
+
+        It 'should export Remove-AtwsAttachment' {$LoadedModule.ExportedCommands['Remove-AtwsAttachment'].Name | Should -Be 'Remove-AtwsAttachment'}
     }
-
-    It 'should export Get-AtwsAttachment' {
-      $LoadedModule.ExportedCommands['Get-AtwsAttachment'].Name | Should -Be 'Get-AtwsAttachment'
-    }
-
-    It 'should export New-AtwsAttachment' {
-      $LoadedModule.ExportedCommands['New-AtwsAttachment'].Name | Should -Be 'New-AtwsAttachment'
-    }
-
-    It 'should export Remove-AtwsAttachment' {
-      $LoadedModule.ExportedCommands['Remove-AtwsAttachment'].Name | Should -Be 'Remove-AtwsAttachment'
-    }
-
-  }
-
 }
 
 Describe 'Issue #36' -Tag 'Issues' {
+    Context 'Issue #36: Date queries with multiple date fields return 0 objects ' { }
 
-  Context 'Issue #36: Date queries with multiple date fields return 0 objects ' { }
-
-  # Issue #35 is a duplicate of issue #38. Or vice versa. But it is already tested...
-  # Issue #34 does not exist 
-
+    # Issue #35 is a duplicate of issue #38. Or vice versa. But it is already tested...
+    # Issue #34 does not exist 
 }
 
-Describe 'Issue #33' -Tag 'Issues' {
-
-  Context 'Issue #33: Updating Diskcache auto running at every import ' { }
-
-}
+Describe 'Issue #33' -Tag 'Issues' {Context 'Issue #33: Updating Diskcache auto running at every import ' { }}
 
 Describe 'Issue #32' -Tag 'Issues' {
+    Context 'Issue #32: Suppress DATE and TIME warning enhancement ' { }
 
-  Context 'Issue #32: Suppress DATE and TIME warning enhancement ' { }
+    # Issue #31 was outside of scope - a parameter wasnt queryable by Autotask design
 
-  # Issue #31 was outside of scope - a parameter wasnt queryable by Autotask design
-
-  # Issue #30: Switch to skip ApiTrackingIdentifier for backward compatibility?  - Deemed out of scope
-
+    # Issue #30: Switch to skip ApiTrackingIdentifier for backward compatibility?  - Deemed out of scope
 }
 
 Describe 'Issue #29' -Tag 'Issues' {
+    Context 'Issue #29: Set-AtwsContact :: Cannot convert Parameter -id from int64[] to int64 bug ' { }
 
-  Context 'Issue #29: Set-AtwsContact :: Cannot convert Parameter -id from int64[] to int64 bug ' { }
-
-  #Issue #28: Set-AtWsTicketCost - Update status - was an API documentation issue
-
+    #Issue #28: Set-AtWsTicketCost - Update status - was an API documentation issue
 }
 
 Describe 'Issue #27' -Tag 'Issues' {
+    Context 'Issue #27: Receiving Confirm prompts with $global:ConfirmPreference="None" ' { }
 
-  Context 'Issue #27: Receiving Confirm prompts with $global:ConfirmPreference="None" ' { }
-
-  # Issue #26: Multiple errors and issues - reporter got it to work on a different computer
-  
+    # Issue #26: Multiple errors and issues - reporter got it to work on a different computer
 }
 
-Describe 'Issue #25' -Tag 'Issues' {
+Describe 'Issue #25' -Tag 'Issues' {Context 'Issue #25: Set-AtwsTicket.ps1 - int32[] to int64 conversion error ' { }}
 
-  Context 'Issue #25: Set-AtwsTicket.ps1 - int32[] to int64 conversion error ' { }
+Describe 'Issue #24' -Tag 'Issues' {Context 'Issue #24: Missing commands, e.g., *-AtwsTicket ' { }}
 
+Describe 'Issue #23' -Tag 'Issues' {Context 'Issue #23: Help w/ picklists lookups in functions and scripts. ' { }}
 
-}
+Describe 'Issue #22' -Tag 'Issues' {Context 'Issue #22: UDF wildcard does not work ' { }}
 
-Describe 'Issue #24' -Tag 'Issues' {
-
-  Context 'Issue #24: Missing commands, e.g., *-AtwsTicket ' { }
-
-}
-
-Describe 'Issue #23' -Tag 'Issues' {
-
-  Context 'Issue #23: Help w/ picklists lookups in functions and scripts. ' { }
-
-}
-
-Describe 'Issue #22' -Tag 'Issues' {
-
-  Context 'Issue #22: UDF wildcard does not work ' { }
-
-}
-
-Describe 'Issue #21' -Tag 'Issues' {
-
-  Context 'Issue #21: New-AtwsContractServiceAdjustment: Get-AtwsData : This entity type does not support the query action. ' { }
-
-}
+Describe 'Issue #21' -Tag 'Issues' {Context 'Issue #21: New-AtwsContractServiceAdjustment: Get-AtwsData : This entity type does not support the query action. ' { }}
 
 Describe 'Issue #20' -Tag 'Issues' {
+    Context 'Issue #20: new-atwscontract: System.dateTime: Can not convert data to date in field ' { }
 
-  Context 'Issue #20: new-atwscontract: System.dateTime: Can not convert data to date in field ' { }
+    # Issue #19: Receiving this error when importing the Autotask Module. I am using an API User. - An API documentation issue related to API version 1.6
 
-  # Issue #19: Receiving this error when importing the Autotask Module. I am using an API User. - An API documentation issue related to API version 1.6
-
-  # Issue #18: The specified module was not loaded because no valid module file was found in any module directory. - Already tested in separate tests
-
+    # Issue #18: The specified module was not loaded because no valid module file was found in any module directory. - Already tested in separate tests
 }
 
-Describe 'Issue #17' -Tag 'Issues' {
+Describe 'Issue #17' -Tag 'Issues' {Context 'Issue #17: Running on Azure Runbooks (Cache) ' { }}
 
-  Context 'Issue #17: Running on Azure Runbooks (Cache) ' { }
+Describe 'Issue #16' -Tag 'Issues' {Context 'Issue #16: Always confirming the write of new autotask data. ' { }}
 
-}
+Describe 'Issue #15' -Tag 'Issues' {Context 'Issue #15: Date errors using filter ' { }}
 
-Describe 'Issue #16' -Tag 'Issues' {
+Describe 'Issue #14' -Tag 'Issues' {Context 'Issue #14: Changing UDF ' { }}
 
-  Context 'Issue #16: Always confirming the write of new autotask data. ' { }
+Describe 'Issue #11' -Tag 'Issues' {Context 'Issue #11: Set "ServiceLevelAgreementID" on contract to nothing ' { }}
 
-}
+Describe 'Issue #10' -Tag 'Issues' {Context 'Issue #10: Unable to connect, as a "\" seems to be prepended ' { }}
 
-Describe 'Issue #15' -Tag 'Issues' {
+Describe 'Issue #9' -Tag 'Issues' {Context 'Issue #9: DateTime conversions ' { }}
 
-  Context 'Issue #15: Date errors using filter ' { }
+Describe 'Issue #8' -Tag 'Issues' {Context 'Issue #8: Set-AtwsAccount -TerritoryID <Integer> Fails  ' { }}
 
-}
+Describe 'Issue #7' -Tag 'Issues' {Context 'Issue #7: Get-AtwsTicket -SubIssueType not updating properly. ' { }}
 
-Describe 'Issue #14' -Tag 'Issues' {
+Describe 'Issue #4' -Tag 'Issues' {Context 'Issue #4: Get / Set AccountManager field ' { }}
 
-  Context 'Issue #14: Changing UDF ' { }
+Describe 'Issue #3' -Tag 'Issues' {Context 'Issue #3: Value does not exist for the required field when using New-AtwsData -InputObject ' { }}
 
-}
-
-Describe 'Issue #11' -Tag 'Issues' {
-
-  Context 'Issue #11: Set "ServiceLevelAgreementID" on contract to nothing ' { }
-
-}
-
-Describe 'Issue #10' -Tag 'Issues' {
-
-  Context 'Issue #10: Unable to connect, as a "\" seems to be prepended ' { }
-
-}
-
-Describe 'Issue #9' -Tag 'Issues' {
-
-  Context 'Issue #9: DateTime conversions ' { }
-
-}
-
-Describe 'Issue #8' -Tag 'Issues' {
-
-  Context 'Issue #8: Set-AtwsAccount -TerritoryID <Integer> Fails  ' { }
-
-}
-
-Describe 'Issue #7' -Tag 'Issues' {
-
-  Context 'Issue #7: Get-AtwsTicket -SubIssueType not updating properly. ' { }
- 
-}
-
-Describe 'Issue #4' -Tag 'Issues' {
-
-  Context 'Issue #4: Get / Set AccountManager field ' { }
-
-}
-
-Describe 'Issue #3' -Tag 'Issues' {
-
-  Context 'Issue #3: Value does not exist for the required field when using New-AtwsData -InputObject ' { }
-
-}
-
-Describe 'Issue #2' -Tag 'Issues' {
-
-  Context 'Issue #2: Filters with parenthesis no longer work ' { }
-
-
-}
+Describe 'Issue #2' -Tag 'Issues' {Context 'Issue #2: Filters with parenthesis no longer work ' { }}
 
 Describe 'Issue #1' -Tag 'Issues' {
+    Context 'Issue #1: Account where a certain field (int) is empty' {
+        $Account = Get-AtwsAccount -id 0
+        $NoAccount = Get-AtwsAccount -id 0 -KeyAccountIcon $null
+        $AccountWithNull = Get-AtwsAccount -id 0 -ParentAccountId $null
 
-  Context 'Issue #1: Account where a certain field (int) is empty' {
-    
-    $Account = Get-AtwsAccount -id 0
-    $NoAccount = Get-AtwsAccount -id 0 -KeyAccountIcon $null
-    $AccountWithNull = Get-AtwsAccount -id 0 -ParentAccountId $null
+        It 'should exist an Account with id 0' {$Account | Should -BeOfType Autotask.Account}
 
-    It 'should exist an Account with id 0' {
-      $Account | Should -BeOfType Autotask.Account
+        It 'should NOT return anything with -KeyAccountIcon $null' {$NoAccount | Should -BeNullOrEmpty}
+
+        It 'should exist an Account with id 0 and a ParentAccountId of $null' {$AccountWithNull | Should -BeOfType Autotask.Account}
     }
-
-    It 'should NOT return anything with -KeyAccountIcon $null' {
-      $NoAccount | Should -BeNullOrEmpty
-    }
-
-    It 'should exist an Account with id 0 and a ParentAccountId of $null' {
-      $AccountWithNull | Should -BeOfType Autotask.Account
-    }
-  }
-
 }
--- Tests/Start-PesterTests.ps1
+++ Tests/Start-PesterTests.ps1
@@ -1,52 +1,52 @@
 <#
 
-    .COPYRIGHT
-    Copyright (c) Office Center Hønefoss AS. All rights reserved. Based on code from Jan Egil Ring (Crayon). Licensed under the MIT license.
-    See https://github.com/officecenter/Autotask/blob/master/LICENSE.md for license information.
+        .COPYRIGHT
+        Copyright (c) Office Center Hønefoss AS. All rights reserved. Based on code from Jan Egil Ring (Crayon). Licensed under the MIT license.
+        See https://github.com/officecenter/Autotask/blob/master/LICENSE.md for license information.
 
-    .SYNOPSIS
-    Start the test set
-    .DESCRIPTION
-    In this, the topmost Pester script I collect necessary parameters and pass them on to the tests that need them.
-    .INPUTS
-    Nothing. This function only takes parameters.
-    .OUTPUTS
-    Nothing.
+        .SYNOPSIS
+        Start the test set
+        .DESCRIPTION
+        In this, the topmost Pester script I collect necessary parameters and pass them on to the tests that need them.
+        .INPUTS
+        Nothing. This function only takes parameters.
+        .OUTPUTS
+        Nothing.
 
 #>
 
 [cmdletbinding(
-  SupportsShouldProcess = $True,
-  ConfirmImpact = 'Low',
-  DefaultParameterSetName = 'Default'
+        SupportsShouldProcess = $True,
+        ConfirmImpact = 'Low',
+        DefaultParameterSetName = 'Default'
 )]
 
 Param
 (
-  [Parameter(
-    Mandatory = $true,
-    ParameterSetName = 'Default'
-  )]
-  [ValidateNotNullOrEmpty()]    
-  [pscredential]
-  $Credential,
+    [Parameter(
+            Mandatory = $True,
+            ParameterSetName = 'Default'
+    )]
+    [ValidateNotNullOrEmpty()]    
+    [pscredential]
+    $Credential,
     
-  [Parameter(
-    Mandatory = $true,
-    ParameterSetName = 'Default'
-  )]
-  [String]
-  $ApiTrackingIdentifier
+    [Parameter(
+            Mandatory = $True,
+            ParameterSetName = 'Default'
+    )]
+    [String]
+    $ApiTrackingIdentifier
 )
 
 
-Import-Module Pester
+Import-Module -Name Pester
 
 
 
 # Move to the folder with your module code and tests
 
-$TestsFolder = Split-Path -Parent $MyInvocation.MyCommand.Path
+$TestsFolder = Split-Path -Parent -Path $MyInvocation.MyCommand.Path
 
 Set-Location  $TestsFolder 
 
@@ -54,25 +54,26 @@ Set-Location  $TestsFolder
 
 # Run the structure test
 
-foreach ($tag in 'Manifest'<#, 'Functions'#>) { 
+foreach ($tag in 'Manifest'<#, 'Functions'#>) 
+{
+    $TestResult = Invoke-Pester "$TestsFolder\Autotask.Module.Validation.Tests.ps1" -Show Fails -Tag $tag -PassThru
 
-  $TestResult = Invoke-Pester "$TestsFolder\Autotask.Module.Validation.Tests.ps1" -Show Fails -Tag $tag -PassThru
-
-  If ($TestResult.PassedCount -ne $TestResult.PassedCount) {
-
-    Throw "Manifest did not validate, execution stopped"
-
-  }
-
+    If ($TestResult.PassedCount -ne $TestResult.PassedCount) {Throw 'Manifest did not validate, execution stopped'}
 }
 # Test module import
-Invoke-Pester -Script @{ 
-  Path       = "$TestsFolder\Autotask.Module.Import.Tests.ps1"; 
-  Parameters = @{ Credential = $Credential; ApiTrackingIdentifier = $ApiTrackingIdentifier } 
+Invoke-Pester -Script @{
+    Path       = "$TestsFolder\Autotask.Module.Import.Tests.ps1"
+    Parameters = @{
+        Credential            = $Credential
+        ApiTrackingIdentifier = $ApiTrackingIdentifier
+    }
 }
 
 # Verify that we still pass all issues
-Invoke-Pester -Script @{ 
-  Path       = "$TestsFolder\Autotask.Module.Issues.Tests.ps1"; 
-  Parameters = @{ Credential = $Credential; ApiTrackingIdentifier = $ApiTrackingIdentifier } 
+Invoke-Pester -Script @{
+    Path       = "$TestsFolder\Autotask.Module.Issues.Tests.ps1"
+    Parameters = @{
+        Credential            = $Credential
+        ApiTrackingIdentifier = $ApiTrackingIdentifier
+    }
 }  -Name 'Issue #43'
