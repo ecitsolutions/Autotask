8666cff267cb5b7332afeefa7fdc8dcedb99be58
hugo <hugo@office-center.no>
2019-12-17 16:47:25 +0100

    Updated copyright after merger

--- Autotask/Private/Get-Copyright.ps1
+++ Autotask/Private/Get-Copyright.ps1
@@ -1,12 +1,12 @@
 ﻿<#
 
 .COPYRIGHT
-Copyright (c) Office Center Hønefoss AS. All rights reserved. Based on code from Jan Egil Ring (Crayon). Licensed under the MIT license.
+Copyright (c) ECIT Solutions AS. All rights reserved. Licensed under the MIT license.
 See https://github.com/officecenter/Autotask/blob/master/LICENSE.md for license information.
 
 #>
 Function Get-Copyright {
-     <#
+    <#
       .SYNOPSIS
 
       .DESCRIPTION
@@ -22,11 +22,14 @@ Function Get-Copyright {
       .LINK
 
   #>
+  
+    Write-Debug ('{0}: Begin of function' -F $MyInvocation.MyCommand.Name)
+
     @"
 <#
 
 .COPYRIGHT
-Copyright (c) Office Center Hønefoss AS. All rights reserved. Based on code from Jan Egil Ring (Crayon). Licensed under the MIT license.
+Copyright (c) ECIT Solutions AS. All rights reserved. Licensed under the MIT license.
 See https://github.com/officecenter/Autotask/blob/master/LICENSE.md for license information.
 
 #>
