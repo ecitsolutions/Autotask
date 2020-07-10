2938728ba946b0e4b2bfeef83d3fe60b66f14f27
Hugo Klemmestad <hugo@och-m-2.local>
2020-07-10 11:32:36 +0200

    Get fieldinfo from api if necessary

--- Autotask/Public/Get-AtwsPicklistValue.ps1
+++ Autotask/Public/Get-AtwsPicklistValue.ps1
@@ -121,13 +121,13 @@ Function Get-AtwsPicklistValue {
         
         if ($UserDefinedFields.IsPresent -and $script:FieldInfoCache[$Entity].EntityInfo.HasUserDefinedFields) {
 
-            $picklistValues = $script:FieldInfoCache[$Entity].UDFInfo.Where{ $_.Name -eq $FieldName }.PicklistValues.where{ $_.IsActive }
+            $picklistValues = (Get-AtwsFieldInfo -Entity $Entity -UserDefinedFields).Where{ $_.Name -eq $FieldName }.PicklistValues.where{ $_.IsActive }
     
             Write-Debug -Message ('{0}: Entity {1} has userdefined fields and user defined field {2} has {3} picklist values.' -F $MyInvocation.MyCommand.Name, $Entity, $FieldName, $result.count) 
         }
         elseIf ($script:FieldInfoCache[$Entity].HasPicklist) { 
     
-            $picklistValues = $script:FieldInfoCache[$Entity].FieldInfo.Where{ $_.Name -eq $FieldName }.PicklistValues.where{ $_.IsActive }
+            $picklistValues = (Get-AtwsFieldInfo -Entity $Entity).Where{ $_.Name -eq $FieldName }.PicklistValues.where{ $_.IsActive }
     
             Write-Debug -Message ('{0}: Entity {1} has picklists and field {2} has {3} picklist values.' -F $MyInvocation.MyCommand.Name, $Entity, $FieldName, $result.count) 
         }
