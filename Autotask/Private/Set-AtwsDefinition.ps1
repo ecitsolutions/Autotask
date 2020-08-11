637fd6f999c78cda377f96b13e955bd7a1b91a26
Hugo Klemmestad <hklemmestad@ecitsolutions.no>
2020-08-11 17:13:02 +0200

    Switch to arraylist for speed

--- Autotask/Private/Set-AtwsDefinition.ps1
+++ Autotask/Private/Set-AtwsDefinition.ps1
@@ -18,7 +18,7 @@
             $VerbosePreference = $Script:Atws.Configuration.VerbosePref
         }
         
-        $ModifiedObjects = @()
+        $ModifiedObjects = [Collections.ArrayList]::new()
     }
 
     process {
@@ -54,7 +54,7 @@
             $processObject = $InputObject | Update-AtwsObjectsWithParameters -BoundParameters $PSBoundParameters -EntityName $EntityName
             
             # If using pipeline this block (process) will run once pr item in the pipeline. make sure to return them all
-            $ModifiedObjects += Set-AtwsData -Entity $processObject
+            [void]$ModifiedObjects.Add((Set-AtwsData -Entity $processObject))
         
         }
     
