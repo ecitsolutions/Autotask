1b6e801f5d7d6e24a59e51e3bf4b021cae09c6d6
Hugo Klemmestad <hugo@och-m-2.local>
2020-08-12 16:05:33 +0200

    Fixed typo

--- Autotask/Private/Get-AtwsParameterDefinition.ps1
+++ Autotask/Private/Get-AtwsParameterDefinition.ps1
@@ -123,8 +123,8 @@ Function Get-AtwsParameterDefinition {
                 }
             }
             'Set' { 
-                if (($Entity.ContainsKey('WritableFields'))) {
-                    [array]$fields = $Entity['WritableFields'] | ForEach-Object {
+                if (($Entity.ContainsKey('WriteableFields'))) {
+                    [array]$fields = $Entity['WriteableFields'] | ForEach-Object {
                         $parameterSet[$_] = @('Input_Object', 'By_parameters', 'By_Id')
                         $Entity['FieldInfo'][$_]
                     }
@@ -143,7 +143,7 @@ Function Get-AtwsParameterDefinition {
     
         # Add Name alias for EntityName parameters
         $entityNameParameter = '{0}Name' -f $Entity.Name
-        foreach ($field in $fields ) {
+        foreach ($field in $fields | sort-object -property name) {
             # Start with native field type
             $Type = $field.Type
 
