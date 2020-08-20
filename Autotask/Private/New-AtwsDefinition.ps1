fc00265310f2d5564fb89e52edec9388e600dd6e
Hugo Klemmestad <hugo@och-m-2.local>
2020-08-20 15:20:05 +0200

    throw errors up to entity function for complete stacktrace

--- Autotask/Private/New-AtwsDefinition.ps1
+++ Autotask/Private/New-AtwsDefinition.ps1
@@ -70,7 +70,19 @@
             # Process parameters and update objects with their values
             $processObject = $processObject | Update-AtwsObjectsWithParameters -BoundParameters $PSBoundParameters -EntityName $EntityName
             
-            $result = Set-AtwsData -Entity $processObject -Create
+            try { 
+                # If using pipeline this block (process) will run once pr item in the pipeline. make sure to return them all
+                $result += Set-AtwsData -Entity $processObject -Create
+            }
+            catch {
+                write-host "ERROR: " -ForegroundColor Red -NoNewline
+                write-host $_.Exception.Message
+                write-host ("{0}: {1}" -f $_.CategoryInfo.Category,$_.CategoryInfo.Reason) -ForegroundColor Cyan
+                $_.ScriptStackTrace -split '\n' | ForEach-Object {
+                    Write-host "  |  " -ForegroundColor Cyan -NoNewline
+                    Write-host $_
+                }
+            }
         }
     }
 
