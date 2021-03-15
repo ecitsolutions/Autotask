be4ebe707bd787512689f3221d8de6f89d3a1b75
Hugo Klemmestad <hklemmestad@ecitsolutions.no>
2021-03-15 14:59:17 +0100

    Fixed setting properties at runtime. This one didnt work at all

--- Autotask/Public/Set-AtwsModuleConfiguration.ps1
+++ Autotask/Public/Set-AtwsModuleConfiguration.ps1
@@ -8,11 +8,19 @@
 Function Set-AtwsModuleConfiguration {
     <#
         .SYNOPSIS
-            This function re-loads the module with the correct parameters for full functionality
+            This function updates the runtime configuration of the module.
         .DESCRIPTION
-            This function is a wrapper that is included for backwards compatibility with previous module behavior.
-            These parameters should be passed to Import-Module -Variable directly, but previously the module 
-            consisted of two, nested modules. Now there is a single module with all functionality.
+            This function updates the runtime configuration of the module. Values that can be changed while the module is loaded
+            are:
+            - Credentials (both username and password may be changed separately)
+            - API key
+            - Whether parameters with picklist values should show labels in place of numbers (ConvertPicklistIdsToLabel)
+            - Debug preference
+            - Verbose preference
+            - Errorlimit - how many errors should Set-Atws* or New-Atws* functions accept before the operation is aborted
+
+            The parameters Prefix and RefreshCache does not have any effect on the current connection. They must be saved and loaded
+            as part of a later connection to have any effect.
         .INPUTS
             A PSCredential object. Required. 
             A string used as ApiTrackingIdentifier. Required. 
@@ -26,21 +34,62 @@ Function Set-AtwsModuleConfiguration {
 	
     [cmdletbinding(
         SupportsShouldProcess = $true,
-        ConfirmImpact = 'Medium'
+        ConfirmImpact = 'Medium',
+        DefaultParameterSetName = 'Username_and_password'
     )]
     Param
-    (
-        [ValidateNotNullOrEmpty()]    
+    ( 
+        [Parameter(
+            ParameterSetName = 'Credentials'
+        )]
+        [ValidateNotNullOrEmpty()] 
         [pscredential]
         $Credential,
-    
+
+        [Parameter(
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'Username_and_password'
+        )]
+        [ValidateNotNullOrEmpty()] 
         [string]
-        $ApiTrackingIdentifier,
+        $Username,
     
-        [Alias('Picklist', 'UsePickListLabel')]
+        [Parameter(
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'Username_and_password'
+        )]
+        [ValidateNotNullOrEmpty()]
+        [securestring]
+        $SecurePassword,
+    
+        [Parameter(
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'Username_and_password'
+        )]
+        [Parameter(
+            ParameterSetName = 'Credentials'
+        )]
+        [ValidateNotNullOrEmpty()]
+        [securestring]
+        $SecureTrackingIdentifier,
+    
+        [Parameter(
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'Username_and_password'
+        )]
+        [Parameter(
+            ParameterSetName = 'Credentials'
+        )]
         [switch]
         $ConvertPicklistIdToLabel,
     
+        [Parameter(
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'Username_and_password'
+        )]
+        [Parameter(
+            ParameterSetName = 'Credentials'
+        )]
         [ValidateScript( {
                 # It can be empty, but if it isn't it should be max 8 characters and only letters and numbers
                 if ($_.length -eq 0 -or ($_ -match '[a-zA-Z0-9]' -and $_.length -gt 0 -and $_.length -le 8)) {
@@ -53,16 +102,48 @@ Function Set-AtwsModuleConfiguration {
         [string]
         $Prefix,
 
+        [Parameter(
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'Username_and_password'
+        )]
+        [Parameter(
+            ParameterSetName = 'Credentials'
+        )]
         [switch]
         $RefreshCache,
 
+        [Parameter(
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'Username_and_password'
+        )]
+        [Parameter(
+            ParameterSetName = 'Credentials'
+        )]
         [ValidateSet('Stop', 'Inquire', 'Continue', 'SilentlyContinue')]
         [string]
         $DebugPref,
-        
+
+        [Parameter(
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'Username_and_password'
+        )]
+        [Parameter(
+            ParameterSetName = 'Credentials'
+        )]
         [ValidateSet('Stop', 'Inquire', 'Continue', 'SilentlyContinue')]
         [string]
-        $VerbosePref
+        $VerbosePref,
+
+        [Parameter(
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'Username_and_password'
+        )]
+        [Parameter(
+            ParameterSetName = 'Credentials'
+        )]
+        [ValidateRange(0, 100)]
+        [int]
+        $ErrorLimit
     )
     
     begin { 
@@ -91,43 +172,50 @@ Function Set-AtwsModuleConfiguration {
   
     process {
 
-        switch ($PSCmdlet.MyInvocation.BoundParameters) { 
-            'Credential' {
-                $Script:Atws.Configuration.Username = $Credential.UserName
-                $Script:Atws.Configuration.SecurePassword = $Credential.Password
-                $Script:Atws.ClientCredentials.UserName.UserName = $Credential.UserName
-                $Script:Atws.ClientCredentials.UserName.Password = $Credential.GetNetworkCredential().Password
-            }
-            'ApiTrackingIdentifier' { 
-                $Script:Atws.integrationsValue.IntegrationCode = $ApiTrackingIdentifier
-            }
-            'ConvertPicklistIdToLabel' {
-                $Script:Atws.Configuration.ConvertPicklistIdToLabel = $ConvertPicklistIdToLabel.IsPresent
-            }
-            'Prefix' { 
-                if ($Prefix -ne $Script:Atws.Configuration.Prefix) { 
-                    Write-Warning "The module prefix cannot be changed while the module is loaded. A module reload is necessary."
-                    $Script:Atws.Configuration.Prefix = $Prefix
+        foreach ($parameter in $PSBoundParameters.GetEnumerator()) { 
+
+            $caption = $MyInvocation.MyCommand.Name
+            $verboseDescription = '{0}: Saving new {1} as requested.' -F $caption, $parameter.key
+            $verboseWarning = '{0}: About to save new {1}. Do you want to continue?' -F $caption, $parameter.key
+
+            if ($PSCmdlet.ShouldProcess($verboseDescription, $verboseWarning, $caption)) {
+                # Only run code if parameter has been use
+                switch ($parameter.key) { 
+                    'Credential' {
+                        $Script:Atws.Configuration.Username = $Credential.UserName
+                        $Script:Atws.Configuration.SecurePassword = $Credential.Password
+                        $Script:Atws.ClientCredentials.UserName.UserName = $Credential.UserName
+                        $Script:Atws.ClientCredentials.UserName.Password = $Credential.GetNetworkCredential().Password
+                    }
+                    'ApiTrackingIdentifier' { 
+                        $Script:Atws.integrationsValue.IntegrationCode = $ApiTrackingIdentifier
+                    }
+                    'ConvertPicklistIdToLabel' {
+                        $Script:Atws.Configuration.ConvertPicklistIdToLabel = $ConvertPicklistIdToLabel.IsPresent
+                    }
+                    'Prefix' { 
+                        if ($Prefix -ne $Script:Atws.Configuration.Prefix) { 
+                            Write-Warning "The module prefix cannot be changed while the module is loaded. A module reload is necessary."
+                            $Script:Atws.Configuration.Prefix = $Prefix
+                        }
+                    }
+                    'RefreshCache' { 
+                        $Script:Atws.Configuration.RefreshCache = $RefreshCache.IsPresent           
+                    }
+                    'DebugPref' { 
+                        $DebugPreference = $DebugPref
+                        $Script:Atws.Configuration.DebugPref = $DebugPref
+                    }
+                    'VerbosePref' {
+                        $VerbosePreference = $VerbosePref
+                        $Script:Atws.Configuration.VerbosePref = $VerbosePref
+                    }
+                    'ErrorLimit' {
+                        $Script:Atws.Configuration.ErrorLimit = $ErrorLimit
+                    }
                 }
-            }
-            'RefreshCache' { 
-                if ($RefreshCache.IsPresent) { 
-                    $Script:Atws.Configuration.RefreshCache = $true
-                    $reloadModule = $true                }
-                else {
-                    $Script:Atws.Configuration.RefreshCache = $false
-                }
-            }
-            'DebugPref' { 
-                $DebugPreference = $DebugPref
-                $Script:Atws.Configuration.DebugPref = $DebugPref
-            }
-            'VerbosePref' {
-                $VerbosePreference = $VerbosePref
-                $Script:Atws.Configuration.VerbosePref = $VerbosePref
             }
         }
-        
     }
   
     end {
