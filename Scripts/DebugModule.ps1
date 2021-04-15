3ba4cd4c7d96c1e1f1c25ccdb25f200ea76e2491
Bjorn Wolstad <bjornwol@hotmail.com>
2021-04-15 17:40:40 +0200

    layout of lines that needs to be debugged.

--- Scripts/DebugModule.ps1
+++ Scripts/DebugModule.ps1
@@ -4,56 +4,74 @@
     This is basically a workflow of what we need to write tests for as well.
 #>
 
+
+Import-Module Pester -RequiredVersion 5.1.0 -ErrorAction Stop
+$PesterModule = Get-Module -Name Pester
+    
 $moduleName = 'Autotask'
 $RootPath = 'C:\Git\Autotask'
 $modulePath = '{0}\{1}' -F $RootPath, $ModuleName
+$SandBoxDomain = '@ECITSOLUTIONSSB12032021.NO'
 
-# if (-not $Global:Credential -or -not $Global:TI) {
-#     [pscredential]$Global:Credential = Get-Credential -UserName 'bautomation@ECITSOLUTIONS.no'
-#     $Global:TI = Read-Host -Prompt 'API TrackingIdentifier'
-# }
+if (-not $Global:Credential -or -not $Global:TI) {
+    Write-Warning "Running pester tests based on defaultconfig."
+    $P = Join-Path (Split-Path -Path $profile -Parent) -ChildPath 'AtwsConfig.clixml'
+    $Settings = Import-Clixml -Path $P -ErrorAction Stop
+    try {
+        $Global:Credential = [pscredential]::new($Settings.Default.Username, $Settings.Default.SecurePassword)
+        $SBUname = $Settings.Default.Username.Split('@')[0] + $SandBoxDomain
+        $Global:SandboxCredential = [pscredential]::new($SBUname, $Settings.Default.SecurePassword)
+        $Global:SecureTI = $Settings.Default.SecureTrackingIdentifier
+    
+        $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Global:SecureTI)
+        $Global:TI = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($BSTR)
+    }
+    catch {
+        $message = "Cannot read default moduleconfig file for pester tests. set this up before running pester test."
+        throw (New-Object System.Configuration.Provider.ProviderException $message)
+    }
+}
+    
+$PesterConfigPath = Join-Path (Split-Path -Path $profile -Parent) -ChildPath 'AtwsPesterConfig.clixml'
+if (Test-Path $PesterConfigPath) {
+    Remove-Item $PesterConfigPath
+}
 
 Import-Module $modulePath -Force -ErrorAction Stop
 $loadedModule = Get-Module $moduleName
 
-$Path = Join-Path (Split-Path -Path $profile -Parent) -ChildPath 'AtwsConfig.clixml'
-if (Test-Path $Path ) {
-    "We have an existing configuration:"
-    $Settings = Import-Clixml -Path $Path
-    $Settings.values
+Connect-AtwsWebAPI -AtwsModuleConfigurationName Sandbox
+
+
+#Error 1:
+$Config = New-AtwsModuleConfiguration -Credential $Global:SandboxCredential -SecureTrackingIdentifier $Global:SecureTI -ErrorLimit 20
+Connect-AtwsWebAPI -AtwsModuleConfiguration $Config
+
+#Error 2:
+$Config = New-AtwsModuleConfiguration -Credential $Global:SandboxCredential -SecureTrackingIdentifier $Global:SecureTI -ErrorLimit 20
+            
+Connect-AtwsWebAPI -AtwsModuleConfigurationName Pester
+
+$Products = Get-AtwsInstalledProduct -Type Firewall
+$cont = $false
+$Products[0].psobject.Properties.name.ForEach{
+    if ($_ -match '#') {
+        $cont = $true
+    }
+}
+if (-not $Cont) {
+    Write-Error "fix this"
 }
 
+#Error 3:
+$Devices = Get-AtwsInstalledProduct -Type Server -Active $true
+$Devices.Count | Should -BeGreaterThan 600
 
-#Region Connect to the module using saved moduleconfiguration.
-Write-Warning '#Region Connect to the module using saved moduleconfiguration.'
-# "Connecting by passing parameters to Connect-AtwsWebAPI"
-# Connect-AtwsWebAPI -Credential $Credential -ApiTrackingIdentifier $TI
-# Get-AtwsAccount -id 0 | Select-Object accountname
+$Req1 = Set-AtwsInstalledProduct -InputObject $Devices -UserDefinedFields @{Name = 'Sist logget inn'; Value = 'Pester UDF test var her.' } -PassThru
 
-"Connecting by using default config"
-# Connect-AtwsWebAPI #-AtwsModuleConfigurationPath C:\Users\bjorn\Documents\PowerShell\AtwsConfig.clixml
-Get-AtwsAccount -id 0 | Select-Object accountname
-#EndRegion
-Write-Warning '#EndRegion'
+# TODO: UseCase: now restore old data. Should be able to run Set-AtwsInstalledProduct -InputObject $Devices right?
+$Req2 = Set-AtwsInstalledProduct -InputObject $Devices -PassThru
+
+$AfterModifications = Get-AtwsInstalledProduct -id $Devices.id
 
 
-#Region Save new module configuration and connect using it
-#TODO: Problem, Function is not exported.
-# Get-AtwsModuleConfiguration -Name Default
-Set-AtwsModuleConfiguration -Username 'bautomation@ECITSOLUTIONSSB12032021.NO' -Name 'SandBox' -ErrorLimit 20
-Save-AtwsModuleConfiguration -Name ''
-Get-AtwsModuleConfiguration -Name 'SandBox'
-Get-AtwsModuleConfiguration -Name Default | Set-AtwsModuleConfiguration -ErrorLimit 19 
-Save-AtwsModuleConfiguration -Name SandBox
-
-
-Connect-AtwsWebAPI -AtwsModuleConfigurationName SandBox
-Get-AtwsAccount -id 0 | Select-Object accountname
-
-
-#Now we need to check that Build-AtwsModule does not mess anything up., ergo run same tests afer build
-
-<#
-    Instructions on how to set up module configurations.
-    1. The first time connecting you will be asked for credentials, since no existing moduleconfiguration will exist.
-#>
